8af87a834925ca7d8b73e30221e214773737b7e5
俺不叫化咕龙 <1801943622@qq.com>
2025-08-16 16:57:32 +0800

    Refactor Mandelbrot image generation for improved performance and color handling

--- src/main.rs
+++ src/main.rs
@@ -13,46 +13,39 @@ const RATIO: f64 = (X_AXIS.max - X_AXIS.min) / (Y_AXIS.max - Y_AXIS.min);
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = (WIDTH as f64 / RATIO) as u32;
 
-const MAX_ITER: usize = 1000;
+const MAX_ITER: u8 = u8::MAX;
 
 fn main() {
-    let image = ImageBuffer::from_fn(WIDTH, HEIGHT, |x, y| {
+    let image = ImageBuffer::from_par_fn(WIDTH, HEIGHT, |x, y| {
         let cx = X_AXIS.map(x as f64 / WIDTH as f64);
         let cy = Y_AXIS.map(y as f64 / HEIGHT as f64);
 
         let c = Complex::new(cx, cy);
         let count = iter_count(c);
 
-        image::Rgb(calc_color(count))
+        image::Rgb(calc_color(count as u8))
     });
 
     image.save("mandelbrot.png").expect("Failed to save image");
 }
 
-fn iter_count(c: Complex<f64>) -> usize {
+fn iter_count(c: Complex<f64>) -> u8 {
     let mut z = Complex::new(0.0, 0.0);
     for i in 0..MAX_ITER {
         if z.abs() > 2.0 {
-            return i;
+            return i as u8;
         }
         z = z * z + c;
     }
     return MAX_ITER;
 }
 
-fn calc_color(count: usize) -> [f32; 3] {
-    const IN_COLOR: [f32; 3] = [1., 1., 1.];
-    const OUT_COLOR: [f32; 3] = [0., 0., 0.];
-    if count == MAX_ITER {
+fn calc_color(ratio: u8) -> [u8; 3] {
+    const IN_COLOR: [u8; 3] = [255, 255, 255];
+    if ratio == MAX_ITER {
         IN_COLOR
     } else {
-        let ratio = dbg!(count as f32 / MAX_ITER as f32);
-
-        [
-            (IN_COLOR[0] - OUT_COLOR[0]) * ratio + OUT_COLOR[0],
-            (IN_COLOR[1] - OUT_COLOR[1]) * ratio + OUT_COLOR[1],
-            (IN_COLOR[2] - OUT_COLOR[2]) * ratio + OUT_COLOR[2],
-        ]
+        [ratio, ratio, ratio]
     }
 }
 
