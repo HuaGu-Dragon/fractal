9b084062c06f1532be14ec42ea0fe9e617db9607
俺不叫化咕龙 <1801943622@qq.com>
2025-08-16 15:46:33 +0800

    Implement Mandelbrot set generation and image saving
    - Added main function to compute Mandelbrot set pixels.
    - Introduced Axis struct for mapping coordinates.
    - Implemented iteration count calculation for Mandelbrot set.
    - Created color mapping based on iteration count.
    - Utilized image crate to generate and save the output image as "mandelbrot.png".

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+use image::ImageBuffer;
+use num::{Complex, complex::ComplexFloat};
+
+const X_AXIS: Axis = Axis {
+    max: 2.0,
+    min: -2.0,
+};
+const Y_AXIS: Axis = Axis {
+    max: 1.5,
+    min: -1.5,
+};
+const RATIO: f64 = X_AXIS.range() / Y_AXIS.range();
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = (WIDTH as f64 / RATIO) as u32;
+
+const MAX_ITER: usize = 100;
+
+const BASE_COLOR: [u8; 3] = [0, 0, 0];
+const MAX_COLOR: [u8; 3] = [255, 255, 255];
+
+fn main() {
+    let mut pixels = vec![[0u8; 3]; (WIDTH * HEIGHT) as usize];
+
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            let cx = X_AXIS.map(x as f64 / WIDTH as f64);
+            let cy = Y_AXIS.map(y as f64 / HEIGHT as f64);
+
+            let c = Complex::new(cx, cy);
+            let count = iter_count(c);
+
+            pixels[(y * WIDTH + x) as usize] = calc_color(count);
+        }
+    }
+
+    let image = ImageBuffer::from_fn(WIDTH, HEIGHT, |x, y| {
+        let pixel = pixels[(y * WIDTH + x) as usize];
+        image::Rgb(pixel)
+    });
+
+    image.save("mandelbrot.png").expect("Failed to save image");
+}
+
+fn iter_count(c: Complex<f64>) -> usize {
+    let mut z = Complex::new(0.0, 0.0);
+    for i in 0..MAX_ITER {
+        if z.abs() > 2.0 {
+            return i;
+        }
+        z = z * z + c;
+    }
+    return MAX_ITER;
+}
+
+fn calc_color(count: usize) -> [u8; 3] {
+    if count == MAX_ITER {
+        MAX_COLOR
+    } else {
+        let ratio = count as f64 / MAX_ITER as f64;
+        [
+            ((MAX_COLOR[0] - BASE_COLOR[0]) as f64 * ratio + BASE_COLOR[0] as f64) as u8,
+            ((MAX_COLOR[1] - BASE_COLOR[1]) as f64 * ratio + BASE_COLOR[1] as f64) as u8,
+            ((MAX_COLOR[2] - BASE_COLOR[2]) as f64 * ratio + BASE_COLOR[2] as f64) as u8,
+        ]
+    }
+}
+
+struct Axis {
+    max: f64,
+    min: f64,
+}
+
+impl Axis {
+    const fn range(&self) -> f64 {
+        self.max - self.min
+    }
+
+    fn map(&self, value: f64) -> f64 {
+        self.min + value * self.range()
+    }
+}
