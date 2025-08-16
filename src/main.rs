0759ea646fa4a760373bc802ddaf831ccab8760f
俺不叫化咕龙 <1801943622@qq.com>
2025-08-16 16:03:23 +0800

    Refactor Axis struct and color calculation for improved clarity and performance

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::ops::{Add, Mul, Sub};
+
 use image::ImageBuffer;
 use num::{Complex, complex::ComplexFloat};
 
-const X_AXIS: Axis = Axis {
+const X_AXIS: Axis<f64> = Axis {
     max: 1.0,
     min: -2.0,
 };
-const Y_AXIS: Axis = Axis { max: 1., min: -1. };
+const Y_AXIS: Axis<f64> = Axis { max: 1., min: -1. };
 const RATIO: f64 = X_AXIS.range() / Y_AXIS.range();
 
 const WIDTH: u32 = 800;
@@ -13,8 +15,8 @@ const HEIGHT: u32 = (WIDTH as f64 / RATIO) as u32;
 
 const MAX_ITER: usize = 1000;
 
-const BASE_COLOR: [u8; 3] = [0, 0, 0];
-const MAX_COLOR: [u8; 3] = [255, 255, 255];
+const BASE_COLOR: [u8; 3] = [200, 200, 230];
+const MAX_COLOR: [u8; 3] = [44, 60, 80];
 
 fn main() {
     let image = ImageBuffer::from_fn(WIDTH, HEIGHT, |x, y| {
@@ -45,26 +47,33 @@ fn calc_color(count: usize) -> [u8; 3] {
     if count == MAX_ITER {
         MAX_COLOR
     } else {
-        let ratio = count as f64 / MAX_ITER as f64;
+        let ratio = dbg!(count as f64 / MAX_ITER as f64);
+
         [
-            ((MAX_COLOR[0] - BASE_COLOR[0]) as f64 * ratio + BASE_COLOR[0] as f64) as u8,
+            ((BASE_COLOR[0] - MAX_COLOR[0]) as f64 * ratio + BASE_COLOR[0] as f64) as u8,
             ((MAX_COLOR[1] - BASE_COLOR[1]) as f64 * ratio + BASE_COLOR[1] as f64) as u8,
             ((MAX_COLOR[2] - BASE_COLOR[2]) as f64 * ratio + BASE_COLOR[2] as f64) as u8,
         ]
     }
 }
 
-struct Axis {
-    max: f64,
-    min: f64,
+struct Axis<T> {
+    max: T,
+    min: T,
 }
 
-impl Axis {
-    const fn range(&self) -> f64 {
+impl<T> Axis<T>
+where
+    T: Copy,
+    T: Add<Output = T>,
+    T: Sub<Output = T>,
+    T: Mul<Output = T>,
+{
+    const fn range(&self) -> T {
         self.max - self.min
     }
 
-    fn map(&self, value: f64) -> f64 {
+    fn map(&self, value: T) -> T {
         self.min + value * self.range()
     }
 }
